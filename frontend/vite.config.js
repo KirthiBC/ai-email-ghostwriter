7b4ada955eb7ada1a52181d07b96c431cf319693
Kirthi <kirthi5454@gmail.com>
2026-08-29 14:24:17 +0530

    Fix relative script path in index.html for Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/login1':'http://localhost:5000',
-      '/signup1':'http://localhost:5000',
-      '/history':'http://localhost:5000',
-      '/generate-email': 'http://localhost:5000',
-    }
-  }
 })
